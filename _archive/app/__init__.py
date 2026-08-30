adf7757e0db126556b098fe82431eea8e9bc5cd2
小鱼 <14550830+little-fishy@user.noreply.gitee.com>
2026-08-30 18:11:01 +0800

    改进: _archive 解除 git 跟踪，本地保留历史不上传远程

--- _archive/app/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2026/7/27 下午 11:06
-@Auth ： Yu
-@File ：__init__.py.py
-@IDE ：PyCharm
-@Intro : 
-"""
--- _archive/test_py/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2026/7/14 下午 10:03
-@Auth ： Yu
-@File ：__init__.py.py
-@IDE ：PyCharm
-@Intro : 
-"""
